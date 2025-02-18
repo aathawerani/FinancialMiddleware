1eabb644a4832b98b664d78e7f90b78d6ed8e47f
aathawerani <athawerani@gmail.com>
2025-02-18 17:23:16 +0500

    fixes

---
+++ FileIO/Test.cpp
@@ -0,0 +1,64 @@
+#include "FileIO.h"
+#include "directory.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    // Directory operations
+    CDirectory dir;
+    const char* folderName = "testFolder";
+    const char* fileName = "testFolder/testFile.txt";
+
+    // Create folder
+    if (!dir.doesFolderExist(folderName)) {
+        if (dir.createFolder(folderName)) {
+            std::cout << "Folder created: " << folderName << std::endl;
+        } else {
+            std::cerr << "Failed to create folder: " << folderName << std::endl;
+        }
+    } else {
+        std::cout << "Folder already exists: " << folderName << std::endl;
+    }
+
+    // File operations
+    CFileIO file(fileName, CFileIO::FLAG_CREATE_ALWAYS);
+
+    // Write to file
+    file.WriteParamLine("This is a test line: %d", 1);
+
+    // Read from file
+    char buffer[MAX_STRING_LENGTH];
+    file.FileSeek(0, std::ios::beg);
+    file.ReadLine(buffer);
+    std::cout << "Read from file: " << buffer << std::endl;
+
+    // Break filename
+    char filename[MAX_STRING_LENGTH];
+    char extension[MAX_STRING_LENGTH];
+    dir.breakFilename(fileName, filename, extension);
+    std::cout << "Filename: " << filename << ", Extension: " << extension << std::endl;
+
+    // Break qualified name
+    char path[MAX_STRING_LENGTH];
+    char fname[MAX_STRING_LENGTH];
+    dir.breakQualifiedName(fileName, path, fname);
+    std::cout << "Path: " << path << ", Filename: " << fname << std::endl;
+
+    // Move file
+    const char* destFolder = "testFolder2";
+    if (!dir.doesFolderExist(destFolder)) {
+        dir.createFolder(destFolder);
+    }
+    if (dir.moveFile(destFolder, folderName, "testFile.txt")) {
+        std::cout << "File moved to: " << destFolder << std::endl;
+    } else {
+        std::cerr << "Failed to move file to: " << destFolder << std::endl;
+    }
+
+    // Get temp path
+    std::string tempPath = dir.getTempPath();
+    std::cout << "Temp path: " << tempPath << std::endl;
+
+    return 0;
+}
